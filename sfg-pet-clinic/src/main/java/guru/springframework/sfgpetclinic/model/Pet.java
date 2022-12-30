0fe5fbd84b4c3f66e7fab86364f780e32bfe5718
kjayathilake <kjayathilake@interactions.com>
2022-12-30 16:06:20 -0500

    Pet clinic POJOs

--- sfg-pet-clinic/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -7,4 +7,28 @@ public class Pet {
     private PetType petType;
     private Owner owner;
     private LocalDate dateOfBirth;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
